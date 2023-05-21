b2e78f5c5dd11e9f91a24251b06b6105ad40e8cf
GulyaevSergey <109235648+GulyaevSergey@users.noreply.github.com>
2023-05-22 00:35:47 +0700

    create differences block

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
 import { VideoPlayer } from "./modules/playVideo";
 import { MainSlider } from "./modules/slider-main";
+import { Difference } from "./modules/difference";
 
 window.addEventListener("DOMContentLoaded", () => {
     const slider = new MainSlider({ btns: ".next", page: ".page" });
     slider.render();
     const player = new VideoPlayer(".showup .play", ".overlay");
     player.init();
+
+    new Difference(".officerold", ".officernew", ".officer__card-item").init();
 });
